6e41e9a7309dba72ade78795ee72074fef70ccfd
juul <jdavelaar0@gmail.com>
2026-09-03 10:57:53 +0200

    #14 code om data mee op te halen en te tonen

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,11 @@
+<script>
+    let {data} = $props()
+    const persons = data.person;
+</script>
+ 
+{#each persons as person}
+    {person.name}<br>
+{/each}
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
