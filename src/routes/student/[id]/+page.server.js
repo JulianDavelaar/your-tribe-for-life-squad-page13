a49e07c4fee8b114b66e6bb35eeb0837fa4a4ca2
juul <jdavelaar0@gmail.com>
2026-09-04 12:59:15 +0200

    #13 onnodige js lijn weggehaald

--- src/routes/student/[id]/+page.server.js
+++ src/routes/student/[id]/+page.server.js
@@ -1,5 +1,3 @@
-/** @type {import('./$types').PageLoad} */
-
 export async function load({ params, fetch }) {
     const res = await fetch(`https://fdnd.directus.app/items/person/${params.id}`);
     const data = await res.json();
