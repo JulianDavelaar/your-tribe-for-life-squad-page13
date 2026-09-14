1fe325129d542bc54e555e0cbe3b348c377bc037
juul <jdavelaar0@gmail.com>
2026-09-14 12:34:07 +0200

    #33 load functie hetzelfde gemaakt /[ID]/page.server.js

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,4 +1,4 @@
-export async function load(){
+export async function load({ params, fetch }) {
     const res = await fetch("https://fdnd.directus.app/items/person?fields=*&filter[squads][squad_id][cohort][_eq]=2627");
     const data = await res.json();
  
